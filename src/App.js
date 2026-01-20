af21566b08239c4ed4979d12b651a4cb6818ccef
beli240392 <beli.240392@gmail.com>
2026-01-20 00:32:42 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -273,11 +273,15 @@ function App() {
                   className="bg-gray-500 text-white px-6 py-2 rounded-lg hover:bg-gray-600 transition font-medium"
                 >
                   Cancelar
-                <button
-                  onClick={() => handleEdit(vehicle)}
-                >
-                  Editar
-                </button>
+                    
+                <div>
+                  <span>Cancelar</span>
+                  <button onClick={() => handleEdit(vehicle)}>
+                    Editar
+                  </button>
+                </div>
+
+               
             </div>
           </div>
         </div>
